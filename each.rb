48382f29ed79e610dcff20f7f9346d325dffbe0a
sarexs <sarex.s@yandex.ru>
2015-12-09 20:48:53 +0200

    Create each.rb

---
+++ each.rb
@@ -0,0 +1,20 @@
+#encoding: cp866
+
+(-2...2).each do |x|
+	puts x
+end
+
+puts "========"
+
+(0...5).each do |x|
+	puts x
+end
+
+puts "========"
+
+
+(10...11).each do |x|
+	puts x
+end
+
+puts "========"
